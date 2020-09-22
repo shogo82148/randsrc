7f4e81ffa99f58fcfa483ff53c6d8f8a50866705
Ichinose Shogo <shogo82148@gmail.com>
2020-09-22 14:37:33 +0900

    implement xoshiro512**

---
+++ xoshiro512ss/xoshiro512ss_test.go
@@ -0,0 +1,43 @@
+package xoshiro512ss_test
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"testing"
+
+	"github.com/shogo82148/randsrc/xoshiro512ss"
+)
+
+func BenchmarkInt63(b *testing.B) {
+	src := xoshiro512ss.New([8]uint64{1})
+	for i := 0; i < b.N; i++ {
+		runtime.KeepAlive(src.Int63())
+	}
+}
+
+func BenchmarkUint64(b *testing.B) {
+	src := xoshiro512ss.New([8]uint64{1})
+	for i := 0; i < b.N; i++ {
+		runtime.KeepAlive(src.Uint64())
+	}
+}
+
+func ExampleSource() {
+	src := xoshiro512ss.New([8]uint64{1})
+	r := rand.New(src)
+	for i := 0; i < 10; i++ {
+		fmt.Println(r.Int())
+	}
+	//Output:
+	// 0
+	// 2880
+	// 2880
+	// 0
+	// 5898240
+	// 5901120
+	// 0
+	// 18119393280
+	// 12666386031577920
+	// 6039800928
+}
